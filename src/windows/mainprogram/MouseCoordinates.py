fb41afcfe15f63db924ca7f23f391ce378463690
Gwiazda Michał ISP_ <B0663274@pl.ing-ad>
2016-09-16 16:39:07 +0100

    Implement Waiter class that will turn on transparentWin when mouse is on the edge of screen

--- src/windows/mainprogram/MouseCoordinates.py
+++ src/windows/mainprogram/MouseCoordinates.py
@@ -7,8 +7,8 @@ class MouseCoordinates():
         print('{}, {}'.format(x, y))
 
 
-# this has to be added in main loop program to use get_mouse_position function:
-# self.Frame.bind('<Motion>', lambda event: MouseCoordinates.get_mouse_position(MouseCoordinates(), event))
+# this has to be added in main loop program to use turn_on_transparentwin function:
+# self.Frame.bind('<Motion>', lambda event: MouseCoordinates.turn_on_transparentwin(MouseCoordinates(), event))
 
 # and import there:
 # from MouseCoordinates import MouseCoordinates
--- src/windows/mainprogram/Waiter.py
+++ src/windows/mainprogram/Waiter.py
@@ -1,17 +1,15 @@
 import Tkinter as Tk
-import Waiter
+import RemoteMouseStart
 from multiprocessing import Process
 
-class TransparentWin (Tk.Tk) :
-    ''' Transparent Tk Window Class '''
+class Waiter(Tk.Tk):
 
-    def __init__ (self) :
+    def __init__(self):
 
         Tk.Tk.__init__(self)
 
         self.screen_width = self.winfo_vrootwidth()
         self.screen_height = self.winfo_vrootheight()
-        self.width_to_destroy_transparantwin = self.screen_width - 2
         print self.screen_width
         print self.screen_height
 
@@ -22,12 +20,6 @@ class TransparentWin (Tk.Tk) :
         self.attributes("-alpha", 0.2)
 
         self.create_transparent_widget()
-        # self.config(cursor="none")
-
-    def get_mouse_position(self, event):
-        pozycjax = self.winfo_pointerx()
-        pozycjay = self.winfo_pointery()
-        print('{},{}'.format(pozycjax, pozycjay))
 
     def create_transparent_widget(self):
         bg = '#3e4134'
@@ -38,11 +30,21 @@ class TransparentWin (Tk.Tk) :
 
         self.x_previous = 0
         self.y_previous = 0
-        self.Frame.bind('<Motion>', lambda event: self.get_mouse_position_delta(event, self.x_previous, self.y_previous))
-        self.Frame.bind('<Button-3>', self.exit)
+        self.Frame.bind('<Motion>', lambda event: self.turn_on_transparentwin(event))
 
         ''' Changes the window's size indirectly. '''
-        self.Frame.configure(width=self.screen_width, height=self.screen_height)
+        self.Frame.configure(width=2, height=self.screen_height)
+
+
+    def turn_on_transparentwin(self, event):
+        x, y = event.x, event.y
+        print('{}, {}'.format(x, y))
+        p1 = Process(target=self.destroy())
+        p2 = Process(target=RemoteMouseStart.TransparentWin().mainloop())
+        p1.start()
+        p2.start()
+        # RemoteMouseStart.TransparentWin().mainloop()
+        # self.destroy()
 
     def get_mouse_position_delta(self, event, x_previous, y_previous):
         x, y = event.x, event.y
@@ -51,22 +53,11 @@ class TransparentWin (Tk.Tk) :
         self.ydelta = y - y_previous
         self.x_previous = x
         self.y_previous = y
-        if x > self.width_to_destroy_transparantwin:
-            p1 = Process(target=self.destroy())
-            p2 = Process(target=Waiter.Waiter().mainloop())
-            p1.start()
-            p2.start()
-            self.destroy()
         print('Parametr x: {}, Parameter y: {}'.format(self.xdelta, self.ydelta))
-
-    def exit (self, event) :
-        self.destroy()
 
 def __run__ () :
 
-    TransparentWin().mainloop()
-
+    Waiter().mainloop()
 
 if __name__ == '__main__' :
-
     __run__()
